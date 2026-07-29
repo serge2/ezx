19393c51891893364ca935a0a88d208e0592531b
Sergii Polkovnikov <serge.polkovnikov@gmail.com>
2026-07-29 21:17:39 +0200

    128K TAP loading: Enter key queue, 80-frame initial pause

--- src/ezx_emulator_128.erl
+++ src/ezx_emulator_128.erl
@@ -21,6 +21,7 @@
 -include("z80_records.hrl").
 -include("ezx_emulator.hrl").
 -include("lib/z80.hrl").
+-include("input/ezx_keyboard.hrl").
 
 -define(TSTATES_PER_FRAME, 69888).
 -define(INT_TSTATE, 32).
@@ -100,6 +101,7 @@ init(CPUModule, MemModule, VideoModule, KeyboardModule, BeeperModule, Rom) ->
         keyboard = KeyboardModule:default()
     }.
 
+%% @doc Load a Z80 v1/v2/v3 snapshot into 128K memory.
 -spec load_z80(#machine_state{}, binary()) -> {ok, #machine_state{}} | {error, {atom(), binary()}}.
 load_z80(Machine, Data) ->
     try ezx_z80:parse(Data) of
@@ -148,24 +150,81 @@ load_z80(Machine, Data) ->
             {error, {z80_load_failed, iolist_to_binary(io_lib:format("~p:~p", [C, E]))}}
     end.
 
+%% @doc Load a TAP file. In 128K mode the machine boots into the menu,
+%% so just press Enter (no need to type LOAD "").
+-spec load_tap(#machine_state{}, binary()) -> {ok, #machine_state{}} | {error, {atom(), binary()}}.
+load_tap(Machine, Data) ->
+    try ezx_tap:parse_blocks(Data) of
+        Blocks ->
+            io:format("TAP: parsed ~p blocks~n", [length(Blocks)]),
+            Q = [
+                {80, release},
+                {3, {set, [?KEY_ENTER]}},
+                {5, release}
+            ],
+            {ok, Machine#machine_state{
+                tape_blocks = Blocks,
+                keyboard_queue = Q
+            }}
+    catch
+        C:E:_S ->
+            {error, {bad_tap_data, iolist_to_binary(io_lib:format("~p:~p", [C, E]))}}
+    end.
+
 %% --- delegation wrappers ---
 
+%% @doc Execute one CPU instruction.
+-spec step(#machine_state{}) -> #machine_state{}.
 step(Machine) -> ezx_emulator:step(Machine).
+
+%% @doc Run one full frame (~69888 T-states).
+-spec run_frame(#machine_state{}) -> #machine_state{}.
 run_frame(Machine) -> ezx_emulator:run_frame(Machine).
+
+%% @doc Render the current frame into a screen bitmap.
+-spec render_frame(#machine_state{}) -> #machine_state{}.
 render_frame(Machine) -> ezx_emulator:render_frame(Machine).
+
+%% @doc Render accumulated beeper PCM.
+-spec render_beeper(#machine_state{}) -> {binary(), #machine_state{}}.
 render_beeper(Machine) -> ezx_emulator:render_beeper(Machine).
+
+%% @doc Load a .sna snapshot (48K format).
+-spec load_sna(#machine_state{}, binary()) -> {ok, #machine_state{}} | {error, {atom(), binary()}}.
 load_sna(Machine, Data) -> ezx_emulator:load_sna(Machine, Data).
-load_tap(Machine, Data) -> ezx_emulator:load_tap(Machine, Data).
+
+%% @doc Press a keyboard key.
+-spec press_key(#machine_state{}, non_neg_integer()) -> #machine_state{}.
 press_key(Machine, Key) -> ezx_emulator:press_key(Machine, Key).
+
+%% @doc Release a keyboard key.
+-spec release_key(#machine_state{}, non_neg_integer()) -> #machine_state{}.
 release_key(Machine, Key) -> ezx_emulator:release_key(Machine, Key).
+
+%% @doc Execute instructions up to a target T-state count.
+-spec run_until_tstates(#machine_state{}, non_neg_integer()) -> #machine_state{}.
 run_until_tstates(Machine, Target) -> ezx_emulator:run_until_tstates(Machine, Target).
+
+%% @doc Read a byte from memory.
+-spec read_byte(#machine_state{}, non_neg_integer()) -> {byte(), #machine_state{}}.
 read_byte(Machine, Addr) -> ezx_emulator:read_byte(Machine, Addr).
+
+%% @doc Write a byte to memory.
+-spec write_byte(#machine_state{}, non_neg_integer(), byte()) -> #machine_state{}.
 write_byte(Machine, Addr, Byte) -> ezx_emulator:write_byte(Machine, Addr, Byte).
+
+%% @doc Read a 16-bit word from memory (little-endian).
+-spec read_word(#machine_state{}, non_neg_integer()) -> {non_neg_integer(), #machine_state{}}.
 read_word(Machine, Addr) -> ezx_emulator:read_word(Machine, Addr).
+
+%% @doc Write a 16-bit word to memory (little-endian).
+-spec write_word(#machine_state{}, non_neg_integer(), non_neg_integer()) -> #machine_state{}.
 write_word(Machine, Addr, Word) -> ezx_emulator:write_word(Machine, Addr, Word).
 
 %% --- internal ---
 
+%% @doc Write Z80 memory pages (3-10) into 128K RAM banks (0-7).
+-spec write_128k_pages(any(), #z80_header{}) -> any().
 write_128k_pages(Mem, H) ->
     Pages = H#z80_header.pages,
     maps:fold(fun
